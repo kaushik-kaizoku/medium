956dba12e19ab4f9318ed09efbb5e984283f0952
KAUSHIK <kaushikhrushikesh514@gmail.com>
2024-09-11 21:17:40 +0530

    added zod validation to backend

--- backend/src/user.ts
+++ backend/src/user.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { sign } from 'hono/jwt'
+import { signupInput } from "@kaushikkaizoku/common/dist"
 
 export const userRouter = new Hono<{
 	Bindings: {
@@ -15,6 +16,13 @@ userRouter.post('/signup', async (c) => {
       datasourceUrl: c.env?.DATABASE_URL,
     }).$extends(withAccelerate())
     const body = await c.req.json()
+    const success = signupInput.safeParse(body)
+    if(!success){
+      c.status(411)
+      return c.json({
+        msg:"wrong inputs"
+      })
+    }
     try{
       const user = await prisma.user.create({
         data : {
---
+++ common/dist/index.d.ts
@@ -0,0 +1,34 @@
+import z from "zod";
+export declare const signupInput: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+}, {
+    email: string;
+    password: string;
+}>;
+export declare const signinInput: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+}, {
+    email: string;
+    password: string;
+}>;
+export declare const blogInput: z.ZodObject<{
+    title: z.ZodString;
+    content: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    title: string;
+    content: string;
+}, {
+    title: string;
+    content: string;
+}>;
+export type SignupInput = z.infer<typeof signupInput>;
+export type SigninInput = z.infer<typeof signinInput>;
+export type BlogInput = z.infer<typeof blogInput>;
