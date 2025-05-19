45e52c87d82418bc0e710636ec0e3804f742bc53
Kaushik Hrushikesh Avuti <134273945+kaushik-kaizoku@users.noreply.github.com>
2025-05-19 16:10:21 +0530

    Update Auth.tsx

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -19,7 +19,6 @@ export const Auth =({type}:{type: "signin"| "signup"}) => {
         try{
             const response = await axios.post(`${BACKEND_URL}/api/v1/user/${type === 'signup'?"signup":"signin"}`, postInputs);
             const jwt = response.data;
-            console.log(jwt)
             localStorage.setItem("token", jwt);
             navigate("/blog")
         }catch(e){
